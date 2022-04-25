8bd650de66aabfaba0ffe19d3d8d3a59dcff9141
Carlos Muñoz Díaz <47306703G@cchs.csic.es>
2022-04-25 12:44:06 +0200

    Cambio de datos + Continuación desarrollo

--- pre/js/charts/charts.js
+++ pre/js/charts/charts.js
@@ -1,6 +1,6 @@
 //Desarrollo de las visualizaciones
 import * as d3 from 'd3';
-import { numberWithCommas2 } from '../helpers';
+import { numberWithCommas3 } from '../helpers';
 import { getInTooltip, getOutTooltip, positionTooltip } from '../modules/tooltip';
 import { setChartHeight } from '../modules/height';
 import { setChartCanvas, setChartCanvasImage } from '../modules/canvas-image';
@@ -8,16 +8,22 @@ import { setRRSSLinks } from '../modules/rrss';
 import { setFixedIframeUrl } from './chart_helpers';
 
 //Colores fijos
-const COLOR_PRIMARY_1 = '#F8B05C', 
-COLOR_PRIMARY_2 = '#E37A42',
+const COLOR_PRIMARY_1 = '#F8B05C',
 COLOR_COMP_1 = '#528FAD', 
 COLOR_COMP_2 = '#AADCE0',
-COLOR_GREY_1 = '#D6D6D6', 
-COLOR_GREY_2 = '#A3A3A3',
-COLOR_ANAG__PRIM_1 = '#BA9D5F', 
+COLOR_ANAG_PRIM_1 = '#BA9D5F', 
 COLOR_ANAG_PRIM_2 = '#9E6C51',
-COLOR_ANAG_PRIM_3 = '#9E3515',
 COLOR_ANAG_COMP_1 = '#1C5A5E';
+
+//Diccionario
+let dictionary = {
+    alimentacion: 'Alimentación',
+    vestido: 'Vestido',
+    vivienda: 'Vivienda',
+    sanidad: 'Sanidad',
+    ocio_cultura: 'Ocio y cultura',
+    resto: 'Resto'
+}
 
 export function initChart(iframe) {
     //Lectura de datos
@@ -25,7 +31,7 @@ export function initChart(iframe) {
         if (error) throw error;
 
         //Declaramos fuera las variables genéricas
-        let margin = {top: 20, right: 20, bottom: 20, left: 85},
+        let margin = {top: 20, right: 20, bottom: 20, left: 65},
             width = document.getElementById('chart').clientWidth - margin.left - margin.right,
             height = document.getElementById('chart').clientHeight - margin.top - margin.bottom;
 
@@ -36,14 +42,28 @@ export function initChart(iframe) {
             .append("g")
               .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
         
-        let gruposGasto = ['Alimentacion','Vestido', 'Vivienda', 'Sanidad', 'Ocio y cultura', 'Resto'];
+        let gruposGasto = ['alimentacion','vestido', 'vivienda', 'sanidad', 'ocio_cultura', 'resto'];
 
         //Ejes X
         let x = d3.scaleLinear()
             .domain([0,100])
             .range([0,width]);
 
-        let xAxis = d3.axisBottom(x).ticks(5);
+        let xAxis = function(svg) {
+            svg.call(d3.axisBottom(x).ticks(5));
+            svg.call(function(g) {
+                g.call(function(g){
+                    g.selectAll('.tick line')
+                        .attr('class', function(d,i) {
+                            if (d == 0) {
+                                return 'line-special';
+                            }
+                        })
+                        .attr('y1', '0')
+                        .attr('y2', `-${height}`)
+                });
+            });
+        } 
         
         svg.append("g")
             .attr("transform", "translate(0," + height + ")")
@@ -54,7 +74,7 @@ export function initChart(iframe) {
             .domain(['total','mas65'])
             .range([0, height]);
         
-        let tickLabels = ['Total', '65 y más años'];
+        let tickLabels = ['Total', '65 y más'];
 
         let yAxis = function(g) {
             g.call(d3.axisLeft(y).tickFormat((d,i) => tickLabels[i]))
@@ -66,7 +86,7 @@ export function initChart(iframe) {
 
         let color = d3.scaleOrdinal()
             .domain(gruposGasto)
-            .range([COLOR_PRIMARY_1, COLOR_COMP_2, COLOR_COMP_1, COLOR_GREY_1, COLOR_OTHER_1, COLOR_OTHER_2]);
+            .range([COLOR_PRIMARY_1, COLOR_COMP_2, COLOR_COMP_1, COLOR_ANAG_COMP_1, COLOR_ANAG_PRIM_1, COLOR_ANAG_PRIM_2]);
 
         let stackedDataGasto = d3.stack()
             .keys(gruposGasto)
@@ -80,15 +100,53 @@ export function initChart(iframe) {
                 .enter()
                 .append("g")
                 .attr("fill", function(d) { return color(d.key); })
+                .attr('class', function(d) {
+                    return 'rect-1 ' + d.key;
+                })
                 .selectAll("rect")
                 .data(function(d) { return d; })
                 .enter()
                 .append("rect")
-                .attr('class','prueba')
+                .attr('class','rect')
                 .attr("y", function(d) { return y(d.data.grupo) + y.bandwidth() / 4; })
                 .attr("x", function(d) { return 0; })
                 .attr("width", function(d) { return x(0); })
                 .attr("height", y.bandwidth() / 2)
+                .on('mouseover', function(d,i,e){
+                    //Opacidad de las barras
+                    let other = svg.selectAll('.rect-1');
+                    let current = this.parentNode.classList[1];
+                    let _this = svg.selectAll(`.${current}`);
+                    
+                    other.each(function() {
+                        this.style.opacity = '0.4';
+                    });
+                    _this.each(function() {
+                        this.style.opacity = '1';
+                    });
+
+                    //Texto
+                    console.log(d);                    
+                    let html = '<p class="chart__tooltip--title">' + dictionary[current] + '</p>' + 
+                        '<p class="chart__tooltip--text">Para este grupo de gasto, el gasto medio representa el x% <b>' + numberWithCommas3(parseFloat(data[1][current]).toFixed(2)) + '%</b> del total en este grupo de población</p>';
+            
+                    tooltip.html(html);
+
+                    //Tooltip
+                    positionTooltip(window.event, tooltip);
+                    getInTooltip(tooltip);
+
+                })
+                .on('mouseout', function(d,i,e) {
+                    //Quitamos los estilos de la línea
+                    let bars = svg.selectAll('.rect-1');
+                    bars.each(function() {
+                        this.style.opacity = '1';
+                    });
+                
+                    //Quitamos el tooltip
+                    getOutTooltip(tooltip); 
+                })
                 .transition()
                 .duration(2000)
                 .attr("x", function(d) { return x(d[0]); })
@@ -96,7 +154,7 @@ export function initChart(iframe) {
         }
 
         function animateChart() {
-            svg.selectAll('.prueba')
+            svg.selectAll('.rect')
                 .attr("y", function(d) { return y(d.data.grupo) + y.bandwidth() / 4; })
                 .attr("x", function(d) { return 0; })
                 .attr("width", function(d) { return x(0); })
